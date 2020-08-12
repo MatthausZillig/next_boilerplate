184adb29822925920c9265c9eb6d6513e1e43acd
Matthaus <matthauszigillig@hotmail.com>
2020-08-11 21:03:14 -0300

    feature: add styled-components config / jest / babel / prettier / react testing library

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,7 @@
+﻿const Main = () => (
+  <main>
+    <h1>React Avançado</h1>
+  </main>
+)
+
+export default Main
